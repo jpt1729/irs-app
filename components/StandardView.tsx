714d8db917c26a550277d444bb7608f195248ccb
John <136768731+jpt1729@users.noreply.github.com>
2024-05-18 11:56:30 -0400

    Making it look nice :)

---
+++ components/StandardView.tsx
@@ -0,0 +1,43 @@
+import type { PropsWithChildren, ReactElement } from 'react';
+import { StyleSheet, useColorScheme } from 'react-native';
+import Animated, {
+  interpolate,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useScrollViewOffset,
+} from 'react-native-reanimated';
+
+import { ThemedView } from '@/components/ThemedView';
+
+const HEADER_HEIGHT = 250;
+
+type Props = PropsWithChildren<{
+}>;
+
+export default function StandardView({
+  children,
+}: Props) {
+
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedView style={styles.content}>{children}</ThemedView>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    height: 250,
+    overflow: 'hidden',
+  },
+  content: {
+    flex: 1,
+    marginTop: 50,
+    padding: 32,
+    gap: 16,
+    overflow: 'hidden',
+  },
+});
